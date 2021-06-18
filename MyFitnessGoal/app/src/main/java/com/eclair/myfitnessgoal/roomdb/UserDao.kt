31bebe0fb9b735fc12d8233edaacf830656f84f7
soubiakulsum <soubiakulsum@gmail.com>
2021-06-18 15:43:23 +0530

    calories

--- MyFitnessGoal/app/src/main/java/com/eclair/myfitnessgoal/roomdb/UserDao.kt
+++ MyFitnessGoal/app/src/main/java/com/eclair/myfitnessgoal/roomdb/UserDao.kt
@@ -21,4 +21,7 @@ interface UserDao {
     @Query("SELECT weight FROM User_Table WHERE uid =:uid")
     fun getWeight(uid: String?): String
 
+    @Query("SELECT userName FROM User_Table WHERE uid =:uid")
+    fun getUserName(uid: String?): String
+
 }
